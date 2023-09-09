a1e4c679c4f08f68ddc77a01c6263d52ba76b878
JennerMaxim <jennersi1remaxim@gmail.com>
2023-09-09 20:24:54 +0300

    Updating the app

--- app/src/main/java/com/example/firstpractice/MainActivity.java
+++ app/src/main/java/com/example/firstpractice/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.firstpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
